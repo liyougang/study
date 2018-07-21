a04138ee51fe548c85481d09c5060618d806756e
ligang <huchangligang@163.com>
2018-07-21 22:17:32 +0800

    add two num add

---
+++ src/main/java/com/gang/leetcode/data/ListNode.java
@@ -0,0 +1,18 @@
+package com.gang.leetcode.data;
+
+/**
+ * @author ligang
+ * @desc
+ * @date 2018/7/21下午3:27
+ **/
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int val){
+        this.val = val;
+    }
+
+}
